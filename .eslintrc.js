6507a9822db7323acb6b9d69a98be89a22ee8a82
Aman Kumar <aman29271@gmail.com>
2020-05-15 23:10:04 +0530

    logger added

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,20 @@ module.exports = {
 		es6: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	extends: ['eslint:recommended', 'prettier'],
+	plugins: ['prettier'],
 	globals: {
 		Atomics: 'readonly',
 		SharedArrayBuffer: 'readonly',
 	},
 	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
 		ecmaVersion: 11,
 	},
-	plugins: ['react'],
 	rules: {
+		'prettier/prettier': 'error',
 		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
 	},
-}
+};
