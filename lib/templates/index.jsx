1f11fbb859c679a04c1b2400c081d2f95c5f0d22
Aman Kumar <aman29271@gmail.com>
2020-05-15 23:09:42 +0530

    logger added

--- lib/templates/index.jsx
+++ lib/templates/index.jsx
@@ -1,15 +1,24 @@
-const React = require("react");
+const React = require('react');
 
 function Index({ data }) {
 	return (
 		<>
 			<h1>File Manager</h1>
 			<ul>
-				{data.map((e) => (
-					<li key={e.uuid}>
-						<a href={e.uuid}>{e.name}</a>
-					</li>
-				))}
+				{data.map((e) => {
+					if (e.type === 'directory') {
+						return (
+							<li key={e.uuid}>
+								<a href={e.uuid}>{e.name}</a>
+							</li>
+						);
+					}
+					if (e.type === 'file') {
+						return (
+							<img src={`/${e.uuid}`} height="100" width="100" key={e.uuid} />
+						);
+					}
+				})}
 			</ul>
 		</>
 	);
--- roadmap.md
+++ roadmap.md
@@ -4,3 +4,7 @@ request uri
     - get the corresponding location
     - check if it's a file =>get the relative dir and use streams to write out
     - check it it's a folder => send the cache if exits with that uuid otherwise cache the response
+
+v8 profiling 
+node --prof --no-logfile-per-isolate --log --logfile=file.log bin.js
+node --prof-process file.log >file.txt
